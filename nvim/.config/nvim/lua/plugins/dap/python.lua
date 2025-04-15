8bab2f82d49c40296a46ccc6b3a5a6d7681f608f
alexeiquickcode <alexei.quick@gmail.com>
2025-04-16 07:43:36 +1000

    feat(debugging): added `debugpy` module support

--- nvim/.config/nvim/lua/plugins/dap/python.lua
+++ nvim/.config/nvim/lua/plugins/dap/python.lua
@@ -3,6 +3,84 @@ local utils = require "utils"
 
 local is_windows = utils.is_windows
 
+local enrich_config = function(finalConfig, on_config)
+  local final_config = vim.deepcopy(finalConfig)
+
+  -- Placeholder expansion for launch directives
+  local placeholders = {
+    ["${file}"] = function(_)
+      return vim.fn.expand("%:p")
+    end,
+    ["${fileBasename}"] = function(_)
+      return vim.fn.expand("%:t")
+    end,
+    ["${fileBasenameNoExtension}"] = function(_)
+      return vim.fn.fnamemodify(vim.fn.expand("%:t"), ":r")
+    end,
+    ["${fileDirname}"] = function(_)
+      return vim.fn.expand("%:p:h")
+    end,
+    ["${fileExtname}"] = function(_)
+      return vim.fn.expand("%:e")
+    end,
+    ["${relativeFile}"] = function(_)
+      return vim.fn.expand("%:.")
+    end,
+    ["${relativeFileDirname}"] = function(_)
+      return vim.fn.fnamemodify(vim.fn.expand("%:.:h"), ":r")
+    end,
+    ["${workspaceFolder}"] = function(_)
+      return vim.fn.getcwd()
+    end,
+    ["${workspaceFolderBasename}"] = function(_)
+      return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    end,
+    ["${env:([%w_]+)}"] = function(match)
+      return os.getenv(match) or ""
+    end,
+  }
+
+  if final_config.envFile then
+    local filePath = final_config.envFile
+    for key, fn in pairs(placeholders) do
+      filePath = filePath:gsub(key, fn)
+    end
+
+    for line in io.lines(filePath) do
+      if not line:match("^%s*#") and line:match("=") then
+        local key, value = line:match("^%s*([^=]+)%s*=%s*(.-)%s*$")
+        if key and value then
+          final_config.env = final_config.env or {}
+          final_config.env[key] = value
+        end
+      end
+    end
+  end
+  on_config(final_config)
+end
+
+
+local function get_module_from_current_file()
+  local file_path = vim.fn.expand("%:p") -- full path of current file
+  local cwd = vim.fn.getcwd()
+  if not file_path:match("%.py$") then
+    return nil
+  end
+
+  -- Trim off the current working directory and .py extension
+  local relative_path = file_path:gsub("^" .. cwd .. "/", ""):gsub("%.py$", "")
+
+  -- Replace / with . to convert to Python module path
+  local module_name = relative_path:gsub("/", ".")
+
+  -- Sanity check
+  if module_name == "" then
+    return nil
+  end
+
+  print("Running module with debugpy:", module_name)
+  return module_name
+end
 --------------------------------------------------------------------------------
 ---- Python configuration ------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -22,6 +100,7 @@ else
     type = "executable",
     command = os.getenv "HOME" .. "/.virtualenvs/debugpy/bin/python",
     args = { "-m", "debugpy.adapter" },
+    enrich_config = enrich_config,
   }
 
   -- NOTE: For Docker contaienrs
@@ -32,6 +111,7 @@ else
   -- }
 end
 
+
 dap.configurations.python = {
   {
     -- The first three options are required by nvim-dap
@@ -39,6 +119,7 @@ dap.configurations.python = {
     request = "launch",
     name = "Launch file",
     console = "integratedTerminal",
+    cwd = vim.fn.getcwd(),
 
     -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
     program = "${file}",
@@ -47,17 +128,12 @@ dap.configurations.python = {
   {
     type = "python",
     request = "launch",
-    name = "RUN X Module (UAT)",
+    name = "RUN Current Module (UAT)",
     console = "integratedTerminal",
-    module = "XXXXXXXXX",
-    -- module = "${relativeFile}", -- WARN: This doesn't work
-    cwd = "${workspaceFolder}/gpu_jobs",
+    module = function() return get_module_from_current_file() end,
+    cwd = "${workspaceFolder}",
     justMyCode = true,
-    env = {
-      ENV = "uat",
-      -- NOTE: envFile:   This is only a property in vscode's version of debugpy (so for nvim we have to pass it as an env var)
-      envFile = "${workspaceFolder}/environment/.env.uat",
-    },
+    envFile = "${workspaceFolder}/environment/.env.uat",
     pythonPath = function() return utils.get_python_executable() end,
   },
   {
